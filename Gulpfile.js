84dba99140f72ea68a975185eb060abdcf487136
Benjamin Dehli <superelg@gmail.com>
2016-05-29 18:52:51 +0200

    Update Gulp tasks and add gulp config example

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,5 @@
+/* Add the gulp-config file and  */
+
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
@@ -9,47 +11,48 @@ var gulp = require('gulp'),
     del = require('del');
 
 var config = require('./gulp-config.json');
+var assets = config.paths.assets;
 
 gulp.task('scripts', function() {
     return gulp.src(config.paths.js)
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
-        .pipe(gulp.dest('./assets/js/'))
+        .pipe(gulp.dest(assets + '/js/'))
         .pipe(rev())
         .pipe(rename({ suffix: '.min' }))
         .pipe(uglify())
-        .pipe(gulp.dest('./assets/js/'))
-        .pipe(rev.manifest({base: './assets', merge: true}))
-        .pipe(gulp.dest('./assets/'));
+        .pipe(gulp.dest(assets + '/js/'))
+        .pipe(rev.manifest({base: assets, merge: true}))
+        .pipe(gulp.dest(assets + '/'));
 });
 
 gulp.task('styles', function() {
     return gulp.src(config.paths.css)
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('main.css'))
-        .pipe(gulp.dest('./assets/css/'))
+        .pipe(gulp.dest(assets + '/css/'))
         .pipe(rev())
         .pipe(rename({ suffix: '.min' }))
         .pipe(cssnano())
-        .pipe(gulp.dest('./assets/css/'))
-        .pipe(rev.manifest({base: './assets', merge: true}))
-        .pipe(gulp.dest('./assets/'));
+        .pipe(gulp.dest(assets + '/css/'))
+        .pipe(rev.manifest({base: assets, merge: true}))
+        .pipe(gulp.dest(assets + '/'));
 });
 
 gulp.task('fonts', function() {
     return gulp.src(config.paths.fonts)
-        .pipe(gulp.dest('./assets/fonts/'));
+        .pipe(gulp.dest(assets + '/fonts/'));
 });
 
 gulp.task('images', function () {
     return gulp.src(config.paths.images)
-        .pipe(gulp.dest('./assets/images/'));
+        .pipe(gulp.dest(assets + '/images/'));
 });
 
 gulp.task('php', function () {
     return gulp.src(config.paths.php)
-        .pipe(gulp.dest('./assets/php/'));
+        .pipe(gulp.dest(assets + '/php/'));
 });
 
 gulp.task('watch', function() {
@@ -60,7 +63,7 @@ gulp.task('watch', function() {
 
 // Clean
 gulp.task('clean', function() {
-    return del(['./assets/css', './assets/js', './assets/fonts', './assets/images', './assets/php']);
+    return del([assets + '/css', assets + '/js', assets + '/fonts', assets + '/images', assets + '/php']);
 });
 
 
