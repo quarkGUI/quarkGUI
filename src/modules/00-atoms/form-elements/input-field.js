3daab1b37bef2ddbad6b1f5acbbc0e583708d8c4
benjamindehli <superelg@gmail.com>
2016-12-20 21:10:00 +0100

    Adjust input field modules

--- src/modules/00-atoms/form-elements/input-field.js
+++ src/modules/00-atoms/form-elements/input-field.js
@@ -1,11 +1,25 @@
 var style = require('./input-field.scss');
 
-const element = `<input type="text" class="${style.input}" placeholder="hoy hoy" />`;
-
 export default function(inputField){
 
+	var id 			= inputField.id 			!== undefined ? inputField.id 			: '';
+	var name 		= inputField.name 			!== undefined ? inputField.name 		: '';
+	var type		= inputField.type 			!== undefined ? inputField.type 		: '';
+	var value		= inputField.value 			!== undefined ? inputField.value 		: '';
+	var placeholder	= inputField.placeholder 	!== undefined ? inputField.placeholder	: '';
+
+	document.addEventListener('DOMContentLoaded', function() {
+		var element = document.getElementById(id) !== undefined ? document.getElementById(id) : false;
+		if (element){
+			element.value ? element.classList.add("is-not-empty") : element.classList.remove("is-not-empty");
+			element.onkeyup = function(){
+				element.value.length ? element.classList.add("is-not-empty") : element.classList.remove("is-not-empty");				
+			};
+		}
+	}, false);
+
 	return `
-		<input id="${inputField.id}" name="${inputField.name}" type="${inputField.type}" class="${style.input}" placeholder="${inputField.placeholder}" />
-	`
+		<input 	id="${id}" name="${name}" type="${type}" value="${value}" placeholder="${placeholder}" class="${style.input}" />
+	`;
 
 }
--- src/modules/01-molecules/form-elements/input-field.js
+++ src/modules/01-molecules/form-elements/input-field.js
@@ -1,19 +1,16 @@
 import InputField from '../../00-atoms/form-elements/input-field'
 var style = require('./input-field.scss');
 
-const element = `
-	<div class="${style.inputGroup}">
-		${InputField}
-		<label for="" class="${style.label}">Label</label>
-	</div>
-`;
 export default function(inputField){
+
+	var id 		= inputField.id 	!== undefined ? inputField.id 		: '';
+	var label	= inputField.label 	!== undefined ? inputField.label 	: '';
 
 	return `
 		<div class="${style.inputGroup}">
 			${InputField(inputField)}
-			<label for="${inputField.id}" class="${style.label}">${inputField.label}</label>
+			<label for="${id}" class="${style.label}">${label}</label>
 		</div>
-	`
+	`;
 
 };
