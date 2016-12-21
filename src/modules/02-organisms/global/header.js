ae1be25d1c24153a44ecca5d2bcb85d9bf2e5e57
benjamindehli <superelg@gmail.com>
2016-12-21 20:01:42 +0100

    Check for 'undefined' in header module

--- src/modules/02-organisms/global/header.js
+++ src/modules/02-organisms/global/header.js
@@ -5,13 +5,21 @@ var style = require('./header.scss');
 
 export default function(headerItems){
 
+	var logoUrl	= headerItems.logo.url !== undefined ? headerItems.logo.url	: '';
+	
+	var logoImage = '';
+	if (headerItems.logo.image !== undefined) logoImage = Image(headerItems.logo.image);
+
+	var listItems = '';
+	if (headerItems.listItems !== undefined) listItems = PrimaryNavigation(headerItems.listItems);
+
 	return `
 		<header class="${style.navbar}">
 			<a class="${style.sidenavToggle}"></a>
-			<a href="${headerItems.logo.url}" class="${style.logo}">
-				${Image(headerItems.logo.image)}
+			<a href="${logoUrl}" class="${style.logo}">
+				${logoImage}
 			</a>
-			${PrimaryNavigation(headerItems.listItems)}
+			${listItems}
 		</header>
 	`
 	
