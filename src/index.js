2197d77f50a8a7fe60feaf6a9043c3b83aba86cb
benjamindehli <superelg@gmail.com>
2017-01-13 21:22:42 +0100

    Add and implement Font Face Observer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+var FontFaceObserver = require('fontfaceobserver');
+var fontObservers = []; 
+var fontFamilies = { 
+	'Lato': [ 
+	{ weight: 'normal' }, 
+	{ weight: 'bold' } 
+	], 
+	'FontAwesome': [ 
+	{ weight: 'normal' } 
+	] 
+} 
+Object.keys(fontFamilies).forEach(function(family) { 
+	fontObservers.push(
+		fontFamilies[family].map(function(config) {
+			return new FontFaceObserver(family, config).load()
+		})
+	); 
+}); 
+
+Promise.all(fontObservers).then(function() { 
+	document.documentElement.classList.add('webfont-loaded'); 
+}, function() { 
+	console.info('Web fonts could not be loaded in time. Falling back to system fonts.'); 
+});
+
+
 
 import TestPage from './modules/04-pages/test-page';
 
