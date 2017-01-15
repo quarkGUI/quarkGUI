3785eceb06c56d4e6bce54955ec7336f08f5d5e1
benjamindehli <superelg@gmail.com>
2017-01-15 21:11:25 +0100

    Add organism module for card

--- src/modules/03-templates/organisms.js
+++ src/modules/03-templates/organisms.js
@@ -2,6 +2,8 @@ var style = require('../../style/globalStyle.scss');
 
 import ActionBar from '../02-organisms/menus/action-bar';
 import ListMenu from '../02-organisms/menus/list-menu';
+import Card from '../02-organisms/cards/card';
+import Grid from '../01-molecules/sections/grid';
 
 
 export default function(){
@@ -86,5 +88,222 @@ export default function(){
 				]
 			}
 		})}
+
+		<h3>Cards</h3>
+		<h4>Card</h4>
+		${Grid({
+			gridItems:[
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card1',
+						title: 'Default card',
+						content: ListMenu({
+							id: 'card-list-menu1',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				},
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card2',
+						title: 'Primary card',
+						theme: 'primary',
+						content: ListMenu({
+							id: 'card-list-menu2',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				},
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card3',
+						title: 'Info card',
+						theme: 'info',
+						content: ListMenu({
+							id: 'card-list-menu3',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				},
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card4',
+						title: 'Success card',
+						theme: 'success',
+						content: ListMenu({
+							id: 'card-list-menu4',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				},
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card5',
+						title: 'Warning card',
+						theme: 'warning',
+						content: ListMenu({
+							id: 'card-list-menu5',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				},
+				{
+					sizes: {
+						xs: '12',
+						sm: '6',
+						md: '4',
+						lg: '4'
+					},
+					content: Card({
+						id: 'card6',
+						title: 'Danger card',
+						theme: 'danger',
+						content: ListMenu({
+							id: 'card-list-menu6',
+							hover: true,
+							listItems: [
+								{
+									title: 'List item with subtitle',
+									subTitle: 'This is a subtitle',
+								},
+								{
+									title: 'List item without subtitle',
+								},
+								{
+									title: 'List item with link and subtitle',
+									subTitle: 'This is a subtitle',
+									link: '#'
+								},
+								{
+									title: 'List item with link',
+									link: '#'
+								},
+							]
+						})
+					})
+				}
+			]
+		})}
 	`	
 }
+
