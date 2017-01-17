1274c578c02a8657c1d964bd8974472bb72e3a1e
benjamindehli <superelg@gmail.com>
2017-01-17 22:52:11 +0100

    Add checkbox and radio button module support for Firefox

--- src/modules/00-atoms/form-elements/checkbox.js
+++ src/modules/00-atoms/form-elements/checkbox.js
@@ -6,8 +6,19 @@ export default function(inputField){
 	var name 		= inputField.name 			!== undefined ? inputField.name 		: '';
 	var value		= inputField.value 			!== undefined ? inputField.value 		: '';
 
+	document.addEventListener('DOMContentLoaded', function() {
+		if (id !== ''){
+			var iconElement = document.getElementById('checkbox-toggle-' + id);
+			var checkboxElement = document.getElementById(id);
+			iconElement.onclick = function(){
+				checkboxElement.checked = checkboxElement.checked ? false : true;		
+			};
+		}
+	}, false);
+
 	return `
 		<input id="${id}" name="${name}" type="checkbox" value="${value}" class="${style.input}" />
+		<span id="checkbox-toggle-${id}" class="${style.checkboxIcon}"></span>
 	`;
 
 }
--- src/modules/00-atoms/form-elements/radio-button.js
+++ src/modules/00-atoms/form-elements/radio-button.js
@@ -6,8 +6,19 @@ export default function(inputField){
 	var name 		= inputField.name 			!== undefined ? inputField.name 		: '';
 	var value		= inputField.value 			!== undefined ? inputField.value 		: '';
 
+	document.addEventListener('DOMContentLoaded', function() {
+		if (id !== ''){
+			var iconElement = document.getElementById('radio-toggle-' + id);
+			var radioElement = document.getElementById(id);
+			iconElement.onclick = function(){
+				radioElement.checked = radioElement.checked ? false : true;		
+			};
+		}
+	}, false);
+
 	return `
 		<input id="${id}" name="${name}" type="radio" value="${value}" class="${style.input}" />
+		<span id="radio-toggle-${id}" class="${style.radioIcon}"></span>
 	`;
 
 }
