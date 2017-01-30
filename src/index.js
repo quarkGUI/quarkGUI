8beb8ae8f77b971bdb8849ecd99e7e37e32f80e5
benjamindehli <superelg@gmail.com>
2017-01-30 23:09:51 +0100

    Adjust font loading

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-var FontFaceObserver = require('fontfaceobserver');
-var fontObservers = []; 
-var fontFamilies = { 
-	'Lato': [ 
-	{ weight: 'normal' }, 
-	{ weight: 'bold' } 
-	], 
-	'FontAwesome': [ 
-	{ weight: 'normal' } 
-	] 
-} 
-Object.keys(fontFamilies).forEach(function(family) { 
-	fontObservers.push(
-		fontFamilies[family].map(function(config) {
-			return new FontFaceObserver(family, config).load()
-		})
-	); 
-}); 
+(function() {
+	if( sessionStorage.foutFontsLoaded ) {
+		document.documentElement.className += " fonts-loaded";
+		return;
+	}
+	
+	var FontFaceObserver = require('fontfaceobserver');
+	var fontObservers = []; 
+	var fontFamilies = { 
+		'Lato': [ 
+		{ weight: 'normal' }, 
+		{ weight: 'bold' } 
+		], 
+		'FontAwesome': [ 
+		{ weight: 'normal' } 
+		] 
+	} 
+	Object.keys(fontFamilies).forEach(function(family) { 
+		fontObservers.push(
+			fontFamilies[family].map(function(config) {
+				return new FontFaceObserver(family, config).load()
+			})
+			); 
+	}); 
 
-Promise.all(fontObservers).then(function() { 
-	document.documentElement.classList.add('webfont-loaded'); 
-}, function() { 
-	console.info('Web fonts could not be loaded in time. Falling back to system fonts.'); 
-});
+	Promise.all(fontObservers).then(function() { 
+		document.documentElement.className += " fonts-loaded";
+
+		sessionStorage.foutFontsLoaded = true;
+	});
+})();
 
 
 import IndexPage from './modules/04-pages/index';
