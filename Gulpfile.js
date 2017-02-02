2a35145b5628fda1af028eb078134fd4a63df654
benjamindehli <superelg@gmail.com>
2017-02-02 21:50:26 +0100

    Adjust path for favicons

--- Gulpfile.js
+++ Gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('generate-favicon', function(done) {
 	realFavicon.generateFavicon({
 		masterPicture: './src/img/svg/quark-GUI-logo-icon.svg',
 		dest: './docs/img/favicons/',
-		iconsPath: '/simple-gui-template/img/favicons/',
+		iconsPath: '/Simple-GUI-Template/img/favicons/',
 		design: {
 			ios: {
 				pictureAspect: 'backgroundAndMargin',
