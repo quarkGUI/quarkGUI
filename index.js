b280db5c36ff7c809d03d1c32861a1087478550a
benjamindehli <superelg@gmail.com>
2017-02-08 20:33:05 +0100

    Restructure functions for modules

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 'use strict';
 
 module.exports = {
-	atoms:     require("./src/modules/atoms"),
-	molecules: require("./src/modules/molecules"),
-	organisms: require("./src/modules/organisms")
+	atoms: {
+		buttons: {
+			actionButton: function(actionButton) {
+				return require("./src/modules/00-atoms/buttons/action-button").default(actionButton);
+			},
+			button: function(button) {
+				return require("./src/modules/00-atoms/buttons/button").default(button);
+			},
+			toggleButton: function(toggleButton) {
+				return require("./src/modules/00-atoms/buttons/toggle-button").default(toggleButton);
+			}
+		},
+		formElements: {
+			checkbox: function(checkbox) {
+				return require("./src/modules/00-atoms/form-elements/checkbox").default(checkbox);
+			},
+			inputField: function(inputField) {
+				return require("./src/modules/00-atoms/form-elements/input-field").default(inputField);
+			},
+			radioButton: function(radioButton) {
+				return require("./src/modules/00-atoms/form-elements/radio-button").default(radioButton);
+			},
+			selectList: function(selectList) {
+				return require("./src/modules/00-atoms/form-elements/select-list").default(selectList);
+			}
+		},
+		media: {
+			image: function(image) {
+				return require("./src/modules/00-atoms/media/image").default(image);
+			}
+		},
+		sections: {
+			gridItem: function(gridItem){
+				return require("./src/modules/00-atoms/sections/grid-item").default(gridItem);
+			}
+		}
+	}
 }
