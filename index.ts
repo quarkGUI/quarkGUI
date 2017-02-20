662117e395270e8c18e746f986111ca7a70829f5
benjamindehli <superelg@gmail.com>
2017-02-20 01:47:16 +0100

    Convert main file to TypeScript

---
+++ index.ts
@@ -0,0 +1,14 @@
+/*!
+ * quarkGUI v0.3.20 (https://github.com/benjamindehli/quarkGUI)
+ * Copyright(c) 2016-2017 Benjamin Dehli (https://github.com/benjamindehli)
+ * Licenced under GNU General Public License
+ */
+
+
+ 'use strict';
+
+ require("font-awesome-webpack!font-awesome-webpack/font-awesome.config.js");
+ 
+ import Modules = require("./src/modules");
+
+ export = Modules;
