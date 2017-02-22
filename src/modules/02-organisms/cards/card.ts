e3b7ff5647babdfe1205a058a5ec7c8f37117f2c
benjamindehli <superelg@gmail.com>
2017-02-22 21:25:16 +0100

    Convert organism module for card to TypeScript

---
+++ src/modules/02-organisms/cards/card.ts
@@ -0,0 +1,46 @@
+const Style = require<any>('./card.scss');
+
+class Card {
+	theme: string = "default";
+	title: string = "";
+	content: string = "";
+	constructor(card: ICard) {
+		if (card.theme !== undefined) this.theme = card.theme;
+		if (card.title !== undefined) this.title = card.title;
+		if (card.content !== undefined) this.content = card.content;
+	}
+
+	private getThemeClass(theme: string){
+		let themeClass = Style.cardThemeDefault;
+		if (theme == 'primary')	themeClass = Style.cardThemePrimary;
+		if (theme == 'info') 	themeClass = Style.cardThemeInfo;
+		if (theme == 'success')	themeClass = Style.cardThemeSuccess;
+		if (theme == 'warning')	themeClass = Style.cardThemeWarning;
+		if (theme == 'danger') 	themeClass = Style.cardThemeDanger;
+		return themeClass;
+	}
+
+	public createModuleElement() {
+		let themeClass: string = this.getThemeClass(this.theme);
+		return `
+			<div class="card ${Style.card} ${themeClass}">
+				<div class="${Style.cardHeader}">
+					<span class="${Style.cardHeaderTitle}">${this.title}</span>
+				</div>
+				<div class="${Style.cardBody}">
+					${this.content}
+				</div>
+			</div>
+		`
+	}
+}
+
+interface ICard {
+	theme?: string;
+	title?: string;
+	content?: string;
+}
+
+export function getModule(card: ICard){
+	return new Card(card).createModuleElement();
+}
