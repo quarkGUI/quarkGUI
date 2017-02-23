4cdf5a956304e6c130cb24748fa26d2a24263e76
benjamindehli <superelg@gmail.com>
2017-02-23 22:33:06 +0100

    Adjust atom module for button

--- src/modules/00-atoms/buttons/button.ts
+++ src/modules/00-atoms/buttons/button.ts
@@ -1,45 +1,55 @@
 const Style = require<any>("./button.scss");
 
 class Button {
-	id: string;
+	id: string = "";
 	link: string = "#";
-	icon: string = '';
-	content: string;
-	typeClass: string = Style.buttonTypeFlat;
-	themeClass: string = Style.buttonThemeDefault;
+	icon: string = "";
+	content: string = "";
+	type?: string = "flat";
+	theme?: string = "default";
+
 	constructor(button: IButton) {
 		this.id = button.id;
+		if (button.id !== undefined) this.id = button.id;
 		if (button.link !== undefined) this.link = button.link;
 		if (button.iconClass !== undefined) this.icon = `<span class="${Style.icon} ${button.iconClass}"></span>`;
 		if (button.content !== undefined ) this.content = button.content;
-		if (button.type !== undefined && button.type == 'raised') this.typeClass = Style.buttonTypeRaised;
-		if (button.theme !== undefined) this.themeClass = this.getThemeClass(button.theme);
+		if (button.type !== undefined) this.type = button.type;
+		if (button.theme !== undefined) this.theme = button.theme;
 	}
 
 	private getThemeClass(theme: string){
-		let themeClass = '';
-		if (theme == 'primary')	themeClass = Style.buttonThemePrimary;
-		if (theme == 'info') 	themeClass = Style.buttonThemeInfo;
-		if (theme == 'success')	themeClass = Style.buttonThemeSuccess;
-		if (theme == 'warning')	themeClass = Style.buttonThemeWarning;
-		if (theme == 'danger') 	themeClass = Style.buttonThemeDanger;
+		let themeClass = Style.buttonThemeDefault;
+		if (theme == "primary")	themeClass = Style.buttonThemePrimary;
+		if (theme == "info") 	themeClass = Style.buttonThemeInfo;
+		if (theme == "success")	themeClass = Style.buttonThemeSuccess;
+		if (theme == "warning")	themeClass = Style.buttonThemeWarning;
+		if (theme == "danger") 	themeClass = Style.buttonThemeDanger;
 		return themeClass;
 	}
 
+	private getTypeClass(type: string){
+		let typeClass = Style.buttonTypeFlat;
+		if (type == "raised") typeClass = Style.buttonTypeRaised;
+		return typeClass;
+	}
+
 	public createModuleElement() {
+		let themeClass = this.getThemeClass(this.theme)
+		let typeClass = (this.type !== undefined) ? this.getTypeClass(this.type) : Style.buttonTypeFlat;
 		return `
-			<a id="${this.id}" href="${this.link}" class="${Style.button} ${this.typeClass} ${this.themeClass}">${this.icon} ${this.content}</a>
+			<a id="${this.id}" href="${this.link}" class="${Style.button} ${typeClass} ${themeClass}">${this.icon} ${this.content}</a>
 		`
 	}
 }
 
 interface IButton {
-    id: string; 
-    type?: string; 
-    theme?: string;
-    link ?: string; 
-    iconClass?: string; 
-    content?: string;
+	id?: string; 
+	type?: string; 
+	theme?: string;
+	link ?: string; 
+	iconClass?: string; 
+	content?: string;
 }
 
 export function getModule(button: IButton){
