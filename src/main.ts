0eacc69ae4ddd6b4b40f959d537468c9e31b2417
benjamindehli <superelg@gmail.com>
2017-03-07 00:17:08 +0100

    Use modules file as main file

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@
  'use strict';
 
  require("font-awesome-webpack!font-awesome-webpack/font-awesome.config.js");
- 
- import * as Modules from "./src/modules";
 
- export = Modules;
+ import * as Atoms from "./modules/atoms";
+ import * as Molecules from "./modules/molecules";
+ import * as Organisms from "./modules/organisms";
+ import * as Globals from "./modules/global";
+
+
+export {Atoms, Molecules, Organisms, Globals}
