9e807df986729a36d8dc99470b69565bfea9c524
benjamindehli <superelg@gmail.com>
2017-05-28 13:38:39 +0200

    Build app

--- lib/modules/01-molecules/messaging/modal.d.ts
+++ lib/modules/01-molecules/messaging/modal.d.ts
@@ -1,13 +1,19 @@
 export declare class Modal {
     id: string;
-    title: string;
-    content: string;
+    triggerElement: string;
+    modalElement: IModalElement;
     constructor(modal: IModal);
+    private addListener(triggerId, targetId);
+    private createTriggerElement();
     createModuleElement(): string;
+}
+export interface IModalElement {
+    content: string;
+    title?: string;
 }
 export interface IModal {
     id: string;
-    title?: string;
-    content: string;
+    triggerElement: string;
+    modalElement: IModalElement;
 }
 export declare function getModule(modal: IModal): string;
