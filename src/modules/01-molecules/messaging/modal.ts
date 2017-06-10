d435a2c47fc547f171fd537f7da44c23153f42f2
benjamindehli <superelg@gmail.com>
2017-06-10 17:52:43 +0200

    Add support for optional buttons in modal footer

--- src/modules/01-molecules/messaging/modal.ts
+++ src/modules/01-molecules/messaging/modal.ts
@@ -1,5 +1,6 @@
 const Style = require<any>("../../../../src/modules/01-molecules/messaging/modal.scss");
 import * as Button from '../../00-atoms/buttons/button'
+import * as ButtonRow from '../../01-molecules/buttons/button-row'
 
 export class Modal {
 	id: string;
@@ -14,6 +15,7 @@ export class Modal {
 		this.modalElement.scrollable = modal.modalElement.scrollable !== undefined ? modal.modalElement.scrollable : false;
 		this.modalElement.fullscreen = modal.modalElement.fullscreen !== undefined ? modal.modalElement.fullscreen : false;
 		this.modalElement.maxWidth = modal.modalElement.maxWidth !== undefined ? modal.modalElement.maxWidth : null;
+		this.modalElement.footerButtons = modal.modalElement.footerButtons !== undefined ? modal.modalElement.footerButtons : null;
 	}
 
 	private addListener(triggerId:string, targetId:string, closeId:string){
@@ -56,13 +58,27 @@ export class Modal {
 		return `<div class='${Style.triggerElementContainer}' id='${this.id}-trigger'>${this.triggerElement}</div>`;
 	}
 
-	private createCloseElement(){
-		let closeButtonElement:string = Button.getModule({
+	private createFooterButtonsElement(){
+		let closeButton:Button.IButton = {
 			id: this.id + '-close',
 			type: 'minimal',
+			theme: 'primary',
 			content: this.modalElement.closeButtontext
-		});
-		return `<div class='${Style.footerButtons}'>${closeButtonElement}</div>`;
+		};
+		let footerButtons: ButtonRow.IButtonRow = {
+			buttons: []
+		}
+		if (this.modalElement.footerButtons !== null){
+			footerButtons = this.modalElement.footerButtons.buttonRow;
+			let beforeCloseButton:boolean = this.modalElement.footerButtons.beforeCloseButton !== undefined ? this.modalElement.footerButtons.beforeCloseButton : false;
+			if (beforeCloseButton){
+				footerButtons.buttons.push(closeButton);
+			}else{
+				footerButtons.buttons.unshift(closeButton);
+			}
+		}
+		let footerButtonsElement:string = ButtonRow.getModule(footerButtons);
+		return `<div class='${Style.footerButtons}'>${footerButtonsElement}</div>`;
 	}
 
 	private setMaxWidth(){
@@ -78,7 +94,7 @@ export class Modal {
 
 	public createModuleElement(){
 		let triggerElement:string = this.createTriggerElement();
-		let closeElement:string = this.createCloseElement();
+		let closeElement:string = this.createFooterButtonsElement();
 		let modalHeaderElement:string = this.modalElement.title !== undefined ? `<div class='${Style.modalHeader}'>${this.modalElement.title}</div>` : '';
 		let scrollableClass:string = this.modalElement.scrollable ? Style.scrollable : '';
 		let fullscreenClass:string = this.modalElement.fullscreen ? Style.fullscreen : '';
@@ -88,6 +104,11 @@ export class Modal {
 	}
 }
 
+export interface IFooterButtons {
+	buttonRow: ButtonRow.IButtonRow;
+	beforeCloseButton?: boolean;
+}
+
 export interface IModalElement {
 	content: string;
 	closeButtontext?: string;
@@ -95,6 +116,7 @@ export interface IModalElement {
 	scrollable?: boolean;
 	fullscreen?: boolean;
 	maxWidth?: string;
+	footerButtons?: IFooterButtons;
 }
 
 export interface IModal {
