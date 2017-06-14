7e0d6d9a030767b2c1c44ffb49eb3e02caacb73d
benjamindehli <superelg@gmail.com>
2017-06-14 21:55:27 +0200

    Bump version

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,5 +1,5 @@
 /*!
- * quarkGUI v0.6.4 (https://github.com/benjamindehli/quarkGUI)
+ * quarkGUI v0.7.0 (https://github.com/benjamindehli/quarkGUI)
  * Copyright(c) 2016-2017 Benjamin Dehli (https://github.com/benjamindehli)
  * Licenced under GNU General Public License
  */
