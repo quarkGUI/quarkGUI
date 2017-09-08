62c88e14bbceef7083d88cfae136510e13053bf1
benjamindehli <superelg@gmail.com>
2017-09-08 22:54:17 +0200

    Adjust positioning of dropdowns near screen edges

--- src/modules/01-molecules/navigation/primary-navigation.ts
+++ src/modules/01-molecules/navigation/primary-navigation.ts
@@ -61,7 +61,7 @@ export class PrimaryNavigation {
 				let responsiveClass = this.getResponsiveClass(listItem);
 				let hasDropdown: boolean = listItem.dropdownContent !== undefined;
 				if (hasDropdown){
-					dropdownContent = `<div class='${Style.dropdownContent}'>${ListNavigation.getModule(listItem.dropdownContent)}<div>`;
+					dropdownContent = `<div class='${Style.dropdownContent}'><span class='${Style.arrow}'></span>${ListNavigation.getModule(listItem.dropdownContent)}</div>`;
 					dropdownClass = `${Style.hasDropdown}`;
 					listElement = `<li class='overlay-element ${responsiveClass} ${dropdownClass}'>${iconElement}<span class='${Style.dropdownTitle}'>${nameElement}</span>${dropdownContent}</li>`;
 				}else{
@@ -82,12 +82,37 @@ export class PrimaryNavigation {
 					let dropdownElements: any = navigationElement.getElementsByClassName(Style.dropdownContent);
 					let dropdownElement: any = dropdownElements[0];
 
-					let navigationElementWidth = navigationElements[i].offsetWidth;
-					let dropdownElementWidth = dropdownElements[0].offsetWidth;
-					let dropdownElementHeight = dropdownElements[0].offsetHeight;
-					let widthDif = navigationElementWidth - dropdownElementWidth;
+					let allreadyCalculated:boolean = dropdownElement.classList.contains("calculated");
 
-					dropdownElement.style.marginLeft = widthDif/2 + 'px';
+					if (!allreadyCalculated){
+
+						let navigationElementWidth = navigationElements[i].offsetWidth;
+						let dropdownElementWidth = dropdownElements[0].offsetWidth;
+						let dropdownElementHeight = dropdownElements[0].offsetHeight;
+						let widthDif:number = dropdownElementWidth - navigationElementWidth;
+
+						let dropdownElementCenterOffset:number = widthDif/2;
+
+						let windowWidth:number = window.innerWidth;
+						let dropdownElementOffsetLeft:number = dropdownElements[0].offsetLeft;
+						let dropdownElementOffsetRigth:number = dropdownElementWidth + dropdownElementOffsetLeft;
+						let dropdownElementHorizontalOverflow:number = dropdownElementOffsetRigth - windowWidth - dropdownElementCenterOffset;
+						if (dropdownElementHorizontalOverflow > 0){
+							dropdownElementCenterOffset = dropdownElementCenterOffset + dropdownElementHorizontalOverflow;
+							
+							// Get arrow element
+							let dropdownArrowElements: any = dropdownElement.getElementsByClassName(Style.arrow);
+							let dropdownArrowElement: HTMLSpanElement = dropdownArrowElements[0];
+
+							// Compensate for margin left on container element and add 5px (half of arrow width)
+							let arrowMarginLeft: number = dropdownElementHorizontalOverflow - 5;
+							dropdownArrowElement.style.marginLeft = arrowMarginLeft + "px";
+
+						}
+						dropdownElement.style.marginLeft = dropdownElementCenterOffset*-1 + 'px';
+						dropdownElement.classList.add("calculated");
+					}
+
 				}
 			}
 		}, false);
