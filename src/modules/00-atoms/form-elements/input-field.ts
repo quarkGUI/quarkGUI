d5dedd86f1e60fa81943066f0b59ba5860ecd973
benjamindehli <superelg@gmail.com>
2017-09-20 23:57:54 +0200

    Add optional vue bindings for input field module

--- src/modules/00-atoms/form-elements/input-field.ts
+++ src/modules/00-atoms/form-elements/input-field.ts
@@ -7,6 +7,7 @@ export class InputField {
 	value: any = "";
 	placeholder: string = "";
 	attributes: string[];
+	vueBindings: IVueBindings;
 	constructor(inputField: IInputField) {
 		this.id = inputField.id;
 		this.name = inputField.name;
@@ -14,6 +15,16 @@ export class InputField {
 		if (inputField.value !== undefined) this.value = inputField.value;
 		if (inputField.placeholder !== undefined) this.placeholder = inputField.placeholder;
 		if (inputField.attributes !== undefined) this.attributes = inputField.attributes;
+		if (inputField.vueBindings !== undefined) this.vueBindings = inputField.vueBindings;
+
+	}
+
+	private getVueBinding(attributeName){
+		let vueBinding = false;
+		if (this.vueBindings !== undefined){
+			vueBinding = this.vueBindings[attributeName] !== undefined ? this.vueBindings[attributeName] : false;
+		}
+		return vueBinding;
 	}
 
 	private addListener(id: string){
@@ -42,10 +53,69 @@ export class InputField {
 	}
 
 	public createModuleElement() {
+		let hasId: boolean = this.id !== undefined || this.getVueBinding('id');
+		let hasName: boolean = this.name !== undefined || this.getVueBinding('name');
+		let hasType: boolean = this.type !== undefined;
+		let hasValue: boolean = this.value !== undefined || this.getVueBinding('value');
+		let hasPlaceholder: boolean = this.placeholder !== undefined || this.getVueBinding('placeholder');
+
+		let idAttribute = '';
+		if (hasId){
+			if (this.getVueBinding('id')){
+				let id = this.getVueBinding('id');
+				idAttribute = `v-bind:id='${id}'`;
+			}else{
+				idAttribute = `id='${this.id}'`;
+			}
+		}
+
+		let nameAttribute = '';
+		if (hasName){
+			if (this.getVueBinding('name')){
+				let name = this.getVueBinding('name');
+				nameAttribute = `v-bind:name='${name}'`;
+			}else{
+				nameAttribute = `name='${this.name}'`;
+			}
+		}
+
+		let typeAttribute = '';
+		if (hasType){
+			typeAttribute = `type='${this.type}'`;
+		}
+
+		let valueAttribute = '';
+		if (hasValue){
+			if (this.getVueBinding('value')){
+				let value = this.getVueBinding('value');
+				valueAttribute = `v-model='${value}'`;
+			}else{
+				valueAttribute = `value='${this.value}'`;
+			}
+		}
+
+		let placeholderAttribute = '';
+		if (hasPlaceholder){
+			if (this.getVueBinding('placeholder')){
+				let placeholder = this.getVueBinding('placeholder');
+				placeholderAttribute = `v-bind:placeholder='${placeholder}'`;
+			}else{
+				placeholderAttribute = `placeholder='${this.placeholder}'`;
+			}
+		}
+		
 		this.addListener(this.id);
+
 		let htmlAtributes: string = this.attributes !== undefined && this.attributes.length ? this.getHtmlAttributes(this.attributes) : '';
-		return `<input id='${this.id}' name='${this.name}' type='${this.type}' value='${this.value}' placeholder='${this.placeholder}' ${htmlAtributes} class='${Style.input}' />`;
+		return `<input ${idAttribute} ${nameAttribute} ${typeAttribute} ${valueAttribute} ${placeholderAttribute} ${htmlAtributes} class='${Style.input}' />`;
 	}
+}
+
+export interface IVueBindings{
+	id?: string;
+	name?: string;
+	value?: string;
+	placeholder?: string;
 }
 
 export interface IInputField{
@@ -55,6 +125,7 @@ export interface IInputField{
 	value?: any;
 	placeholder?: string;
 	attributes?: string[];
+	vueBindings?: IVueBindings;
 }
 
 export function getModule(inputField: IInputField){
--- src/modules/01-molecules/form-elements/input-field.ts
+++ src/modules/01-molecules/form-elements/input-field.ts
@@ -10,6 +10,7 @@ export class InputField extends AtomInputField.InputField{
 		if (inputField.placeholder !== undefined) this.placeholder = inputField.placeholder;
 		if (inputField.attributes !== undefined) this.attributes = inputField.attributes;
 		if (inputField.label !== undefined) this.label = inputField.label;
+		if (inputField.vueBindings !== undefined) this.vueBindings = inputField.vueBindings;
 	}
 
 
@@ -20,7 +21,8 @@ export class InputField extends AtomInputField.InputField{
 			type: this.type,
 			value: this.value,
 			placeholder: this.placeholder,
-			attributes: this.attributes
+			attributes: this.attributes,
+			vueBindings: this.vueBindings
 		}
 		return `<div class='${Style.inputGroup}'>${AtomInputField.getModule(inputField)}<label for='${this.id}' class='${Style.label}'>${this.label}</label></div>`;
 	}
