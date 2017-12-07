9aebf6310ed762f894331d909276fe11526fc15f
Benjamin Dehli <benjamin@arkitektum.no>
2017-12-08 00:20:32 +0100

    Add support for dynamically loaded modals

--- src/modules/01-molecules/messaging/modal.ts
+++ src/modules/01-molecules/messaging/modal.ts
@@ -2,12 +2,17 @@ const Style = require<any>("../../../../src/modules/01-molecules/messaging/modal
 import * as Button from '../../00-atoms/buttons/button'
 import * as ButtonRow from '../../01-molecules/buttons/button-row'
 
+export let counter = 0;
+export let eventListeners:string[] = [];
+
 export class Modal {
 	id: string;
 	triggerElement: string;
 	modalElement: IModalElement;
+	vueBindings: IVueBindings;
 	constructor(modal: IModal) {
-		this.id = modal.id;
+		counter += 1;
+		this.id = 'quark-modal-' + counter;
 		this.triggerElement = modal.triggerElement;
 		this.modalElement = modal.modalElement;
 		if (modal.modalElement.title !== undefined) this.modalElement.title = modal.modalElement.title;
@@ -16,55 +21,136 @@ export class Modal {
 		this.modalElement.fullscreen = modal.modalElement.fullscreen !== undefined ? modal.modalElement.fullscreen : false;
 		this.modalElement.maxWidth = modal.modalElement.maxWidth !== undefined ? modal.modalElement.maxWidth : null;
 		this.modalElement.footerButtons = modal.modalElement.footerButtons !== undefined ? modal.modalElement.footerButtons : null;
+		if (modal.vueBindings !== undefined) this.vueBindings = modal.vueBindings;
 	}
 
-	private addListener(triggerId:string, targetId:string, closeId:string){
-		document.addEventListener('DOMContentLoaded', function() {
-			let elementsIsDefined: boolean = document.getElementById(triggerId) !== undefined && document.getElementById(targetId) !== undefined && document.getElementById(closeId) !== undefined;
-			let elementsIsNotNull: boolean = document.getElementById(triggerId) !== null && document.getElementById(targetId) !== null && document.getElementById(closeId) !== null;
-			if (elementsIsDefined && elementsIsNotNull){
-				let triggerElement:HTMLElement = document.getElementById(triggerId);
-				let targetElement:HTMLElement = document.getElementById(targetId);
-				let closeElement:HTMLElement = document.getElementById(closeId);
+	private getVueBinding(attributeName){
+		let vueBinding = false;
+		if (this.vueBindings !== undefined){
+			vueBinding = this.vueBindings[attributeName] !== undefined ? this.vueBindings[attributeName] : false;
+		}
+		return vueBinding;
+	}
 
-				triggerElement.onclick = function(){
-					if (triggerElement.classList.contains('active')){
-						triggerElement.classList.remove('active');
-						targetElement.classList.remove('active');
-					}else{
-						triggerElement.classList.add('active');
-						targetElement.classList.add('active');
-					}
-				}
-				targetElement.onclick = function(event:any){
-					let targetClassListIsDefined:boolean = event.target.classList !== undefined;
-					if (targetClassListIsDefined){
-						let targetClassList:DOMTokenList = event.target.classList;
-						if (targetClassList.contains(Style.modalOverlay)){
+	public initModal(targetId){
+		document.addEventListener('DOMContentLoaded', function() {
+
+				//let targetId:string = this.getVueBinding('id') ? '' + this.getVueBinding('id') : this.id;
+				let triggerId:string = targetId + '-trigger';
+				let closeId:string = targetId + '-close';
+
+				let elementsIsDefined: boolean = document.getElementById(triggerId) !== undefined && document.getElementById(targetId) !== undefined && document.getElementById(closeId) !== undefined;
+				let elementsIsNotNull: boolean = document.getElementById(triggerId) !== null && document.getElementById(targetId) !== null && document.getElementById(closeId) !== null;
+				if (elementsIsDefined && elementsIsNotNull){
+					let triggerElement:HTMLElement = document.getElementById(triggerId);
+					let targetElement:HTMLElement = document.getElementById(targetId);
+					let closeElement:HTMLElement = document.getElementById(closeId);
+
+
+					triggerElement.onclick = function(){
+						if (triggerElement.classList.contains('active')){
 							triggerElement.classList.remove('active');
 							targetElement.classList.remove('active');
+						}else{
+							triggerElement.classList.add('active');
+							targetElement.classList.add('active');
 						}
 					}
+					targetElement.onclick = function(event:any){
+						let targetClassListIsDefined:boolean = event.target.classList !== undefined;
+						if (targetClassListIsDefined){
+							let targetClassList:DOMTokenList = event.target.classList;
+							if (targetClassList.contains(Style.modalOverlay)){
+								triggerElement.classList.remove('active');
+								targetElement.classList.remove('active');
+							}
+						}
+					}
+					closeElement.onclick = function(event:any){
+						triggerElement.classList.remove('active');
+						targetElement.classList.remove('active');
+					}
 				}
-				closeElement.onclick = function(event:any){
-					triggerElement.classList.remove('active');
-					targetElement.classList.remove('active');
+			}, false);
+	}
+
+	private initAllModals(){
+		let self = this;
+		document.addEventListener('quarkLazyLoaded', function (){
+			let self = this;
+			let targetElements = document.getElementsByClassName(Style.modalOverlay);
+			for (var i = 0; i < targetElements.length; i++) {
+
+				let targetId = targetElements[i].id;
+
+				let triggerId:string = targetId + '-trigger';
+				let closeId:string = targetId + '-close';
+
+				let elementsIsDefined: boolean = document.getElementById(triggerId) !== undefined && document.getElementById(targetId) !== undefined && document.getElementById(closeId) !== undefined;
+				let elementsIsNotNull: boolean = document.getElementById(triggerId) !== null && document.getElementById(targetId) !== null && document.getElementById(closeId) !== null;
+				if (elementsIsDefined && elementsIsNotNull){
+					let triggerElement:HTMLElement = document.getElementById(triggerId);
+					let targetElement:HTMLElement = document.getElementById(targetId);
+					let closeElement:HTMLElement = document.getElementById(closeId);
+
+
+					triggerElement.onclick = function(){
+						if (triggerElement.classList.contains('active')){
+							triggerElement.classList.remove('active');
+							targetElement.classList.remove('active');
+						}else{
+							triggerElement.classList.add('active');
+							targetElement.classList.add('active');
+						}
+					}
+
+					targetElement.onclick = function(event:any){
+						let targetClassListIsDefined:boolean = event.target.classList !== undefined;
+						if (targetClassListIsDefined){
+							let targetClassList:DOMTokenList = event.target.classList;
+							if (targetClassList.contains(Style.modalOverlay)){
+								triggerElement.classList.remove('active');
+								targetElement.classList.remove('active');
+							}
+						}
+					}
+					closeElement.onclick = function(event:any){
+						triggerElement.classList.remove('active');
+						targetElement.classList.remove('active');
+					}
 				}
 			}
 		}, false);
 	}
 
-	private createTriggerElement(){
-		return `<div class='${Style.triggerElementContainer}' id='${this.id}-trigger'>${this.triggerElement}</div>`;
+
+	private addListener(){
+		let self = this;
+		this.initModal(this.id);
+
+		if (!eventListeners.includes('quarkLazyLoaded')){
+			this.initAllModals();
+			eventListeners.push('quarkLazyLoaded');
+		}
+	}
+
+	private createTriggerElement(triggerIdAttribute){
+		return `<div class='${Style.triggerElementContainer}' ${triggerIdAttribute}>${this.triggerElement}</div>`;
 	}
 
 	private createFooterButtonsElement(){
 		let closeButton:Button.IButton = {
-			id: this.id + '-close',
 			type: 'minimal',
 			theme: 'primary',
 			content: this.modalElement.closeButtontext
 		};
+		if (this.getVueBinding('id')){
+			let idBinding = this.getVueBinding('id');
+			closeButton.vueBindings = {id: `${idBinding} + \"-close\"`};
+		}else{
+			closeButton.id = this.id + '-close';
+		}
+
 		let footerButtons: ButtonRow.IButtonRow = {
 			buttons: [closeButton]
 		}
@@ -93,15 +179,37 @@ export class Modal {
 	}
 
 	public createModuleElement(){
-		let triggerElement:string = this.createTriggerElement();
+		let hasId: boolean = this.id !== undefined || this.getVueBinding('id');
+
+		let overlayIdAttribute: string = '';
+		let modalIdAttribute: string = '';
+		let triggerIdAttribute: string = '';
+		if (hasId){
+			if (this.getVueBinding('id')){
+				let id = this.getVueBinding('id');
+				overlayIdAttribute = `v-bind:id='${id}'`;
+				modalIdAttribute = `v-bind:id='${id} + "-modal"'`;
+				triggerIdAttribute = `v-bind:id='${id} + "-trigger"'`;
+			}else{
+				overlayIdAttribute = `id='${this.id}'`;
+				modalIdAttribute = `id='${this.id}-modal'`;
+				triggerIdAttribute = `id='${this.id}-trigger'`;
+			}
+		}
+
+		let triggerElement:string = this.createTriggerElement(triggerIdAttribute);
 		let closeElement:string = this.createFooterButtonsElement();
 		let modalHeaderElement:string = this.modalElement.title !== undefined ? `<div class='${Style.modalHeader}'>${this.modalElement.title}</div>` : '';
 		let scrollableClass:string = this.modalElement.scrollable ? Style.scrollable : '';
 		let fullscreenClass:string = this.modalElement.fullscreen ? Style.fullscreen : '';
 		this.setMaxWidth();
-		this.addListener(this.id + '-trigger', this.id, this.id + '-close');
-		return `${triggerElement} <div id='${this.id}' class='${Style.modalOverlay} ${fullscreenClass}'><div id="${this.id}-modal" class='${Style.modal} ${scrollableClass}'><div class='${Style.modalContainer}'>${modalHeaderElement}<div class='${Style.modalContent}'>${this.modalElement.content}</div><div class='${Style.modalFooter}'>${closeElement}</div></div></div></div>`;
+		this.addListener();
+		return `${triggerElement} <div ${overlayIdAttribute} class='${Style.modalOverlay} ${fullscreenClass}'><div ${modalIdAttribute} class='${Style.modal} ${scrollableClass}'><div class='${Style.modalContainer}'>${modalHeaderElement}<div class='${Style.modalContent}'>${this.modalElement.content}</div><div class='${Style.modalFooter}'>${closeElement}</div></div></div></div>`;
 	}
+}
+
+export interface IVueBindings{
+	id?: string;
 }
 
 export interface IFooterButtons {
@@ -121,11 +229,12 @@ export interface IModalElement {
 
 export interface IModal {
 	id: string;
-	triggerElement: string,
-	modalElement: IModalElement
-	
+	triggerElement: string;
+	modalElement: IModalElement;
+	vueBindings?: IVueBindings;
 }
 
 export function getModule(modal: IModal){
 	return new Modal(modal).createModuleElement();
 }
+
