41bd5bf624b15a13783700e5975460a08ec8f722
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-19 23:05:57 +0100

    Use ListNavigation module in sidebar navigation

--- src/modules/01-molecules/navigation/sidebar-navigation.ts
+++ src/modules/01-molecules/navigation/sidebar-navigation.ts
@@ -3,32 +3,14 @@ import * as ListNavigation from './list-navigation';
 const Style = require<any>('../../../../src/modules/01-molecules/navigation/sidebar-navigation.scss');
 
 export class SidebarNavigation {
-	listItems: IListItem[];
+	listItems: ListNavigation.IListItem[];
 	constructor(sidebarNavigation: ISidebarNavigation) {
 		this.listItems = sidebarNavigation.listItems;
 	}
 
 	private createListItemElements(listItems){
 		let listItemElements = "";
-		for (let listItem of this.listItems){
-			let id = listItem.id !== undefined ? `id='${listItem.id}'` : '';
-			let hasModuleLink: boolean = listItem.moduleLink !== undefined;
-			let hasLink: boolean = listItem.link !== undefined;
-			let hasIcon: boolean = listItem.iconClass !== undefined;
-
-			let linkAttribute: string = '';
-			let iconElement: string = '';
-			if (hasModuleLink){
-				linkAttribute = `data-module-target='${listItem.moduleLink}'`;
-			}else if(hasLink){
-				linkAttribute = `href='${listItem.link}'`;
-			}
-
-			if (hasIcon){
-				iconElement = `<span class='${listItem.iconClass} ${Style.listItemIcon}'></span>`;
-			}
-			listItemElements += `<li><a class='loadPage' ${id} ${linkAttribute}>${iconElement}${listItem.name}</a></li>`;
-		}
+		listItemElements = ListNavigation.getModule({listItems: listItems});
 		return listItemElements;
 	}
 
@@ -39,13 +21,8 @@ export class SidebarNavigation {
 	}
 }
 
-export interface IListItem extends ListNavigation.IListItem{
-	id?: string;
-	moduleLink?: string;
-}
-
 export interface ISidebarNavigation {
-	listItems: IListItem[];
+	listItems: ListNavigation.IListItem[];
 }
 
 export function getModule(sidebarNavigation: ISidebarNavigation){
