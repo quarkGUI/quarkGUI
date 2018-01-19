d795c9ebaff0da47296c104e4296353104762cf4
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-19 23:09:15 +0100

    Add support for vue-router links in list navigation module

--- src/modules/01-molecules/navigation/list-navigation.ts
+++ src/modules/01-molecules/navigation/list-navigation.ts
@@ -11,11 +11,17 @@ export class ListNavigation {
 		if (this.listItems.length){
 			for (let listItem of this.listItems){ 
 				let hasIcon:boolean = listItem.iconClass !== undefined;
+				let isVueRouterLink:boolean = listItem.vueRouterLink !== undefined ? listItem.vueRouterLink : false;
 				let icon:string = '';
+				let link:string = listItem.link !== undefined ? listItem.link : '';
 				if (hasIcon){
 					icon = `<span class='${listItem.iconClass} ${Style.listItemIcon}'></span>`;
 				}
-				listItemElements += `<li><a href='${listItem.link}'>${icon}${listItem.name}</a></li>`;
+				if (isVueRouterLink){
+					listItemElements += `<li><router-link v-bind:to='${link}'>${icon}${listItem.name}</router-link></li>`;
+				}else{
+					listItemElements += `<li><a href='${link}'>${icon}${listItem.name}</a></li>`;
+				}
 			}
 		}
 		return `<ul class='${Style.listNavigation}'>${listItemElements}</ul>`;
@@ -24,8 +30,9 @@ export class ListNavigation {
 
 export interface IListItem {
 	name: string;
-	link: string;
+	link?: string;
 	iconClass?: string;
+	vueRouterLink?: boolean;
 }
 
 export interface IListNavigation{
