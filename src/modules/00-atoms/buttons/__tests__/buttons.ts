c5573c75af1df537161772072cd1df0154acbcdf
Benjamin Dehli <benjamin@arkitektum.no>
2018-01-25 20:34:26 +0100

    Add tests for button module

--- src/modules/00-atoms/buttons/__tests__/buttons.ts
+++ src/modules/00-atoms/buttons/__tests__/buttons.ts
@@ -1,27 +1,21 @@
 import * as Button from '../button';
 
 
-const buttonElement = Button.getModule({
-	id: 'testbutton',
-	submit: true
-});
-
-document.body.innerHTML = buttonElement;
-
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+	expect(1 + 2).toBe(3);
 });
 
-/*
-test('has id testbutton', () => {
-  expect(buttonElement).toContain("id='testbutton'");
+
+it('renders as an anchor with id value "testbutton"', () => {
+	const buttonElement = Button.getModule({
+		id: 'testbutton'
+	});
+	expect(buttonElement).toMatchSnapshot();
 });
 
-test('is submit type', () => {
-  expect(buttonElement).toContain("type='skubmit'");
+it('renders a button anchor with type value "submit"', () => {
+	const buttonElement = Button.getModule({
+		submit: true
+	});
+	expect(buttonElement).toMatchSnapshot();
 });
-
-test('is submit type', () => {
-	let element = document.getElementById('testbutton');
-  expect(element.attributes).toContain('hoy');
-});*/
