2dc677ee9f27b3e82ff7c4bc691d6773e4c739fc
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-03 19:16:55 +0100

    Add entry file

---
+++ src/lib/index.js
@@ -0,0 +1,11 @@
+'use strict';
+import WebFont from 'webfontloader';
+import Button from './components/Buttons/Button';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:200,300,400', 'sans-serif']
+  }
+});
+
+export {Button}
