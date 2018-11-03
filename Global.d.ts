a163b6683b82434861bfe62fababdb77bae462af
Benjamin Dehli <benjamin@arkitektum.no>
2018-11-03 18:58:33 +0100

    Add configurations for React

--- Global.d.ts
+++
@@ -1,5 +0,0 @@
-declare const require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
