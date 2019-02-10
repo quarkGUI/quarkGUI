c350f944429c094a537f22feacdd3c93dff7b889
Benjamin Dehli <benjamin@arkitektum.no>
2019-02-10 18:10:16 +0100

    Adjust global assets

--- src/styleguide/styleguide.js
+++ src/styleguide/styleguide.js
@@ -1,5 +1,9 @@
 import WebFont from 'webfontloader';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faSquare, faDotCircle, faCircle } from '@fortawesome/free-regular-svg-icons'
 import styles from './styleguide.scss';
+
+library.add(faCheckSquare, faSquare, faDotCircle, faCircle)
 
 WebFont.load({
   google: {
